bc87ed46f7dcd4c86c7860803212f555067d77d4
Nobuhisa Mizue <mizue3_1@yahoo.co.jp>
2012-11-16 21:27:11 -0500

    sec

--- app/controllers/ballpark_controller.rb
+++ app/controllers/ballpark_controller.rb
@@ -1,13 +1,16 @@
 class BallparkController < ApplicationController
   def index
     @rdc_servers = RdcServer.all(:order => "typename")
-    @rs = params[:rs].inspect
+    # @rs = params[:rs].inspect
     render
   end
 
   def calc
+    @rdc_servers = RdcServers.all(:order => "typenames)
     @rs = params[:rs].inspect
-    render :json => {:result => @rs}
+    for n in params[:rs]
+      cost = cost + n['num']
+    render :json => {:result => @rs + cost}
   end
 
 end
